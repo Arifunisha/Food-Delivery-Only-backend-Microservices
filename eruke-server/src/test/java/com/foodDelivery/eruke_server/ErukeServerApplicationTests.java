badd7999e0514485d3dedb74a36c65fe5a84d8bb
Arifunisha <arifunisha.n.cseacet@gmail.com>
2026-09-04 17:19:33 +0530

    Add files via upload

---
+++ eruke-server/src/test/java/com/foodDelivery/eruke_server/ErukeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodDelivery.eruke_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErukeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
